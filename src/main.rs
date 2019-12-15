d6de8cc6303c36348b56a19e1ed2122806312aa0
Daniel Sockwell <dsockwell@gmail.com>
2019-12-15 17:16:41 -0500

    WIP with `.possible_values`

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{crate_version, App};
+use clap::{crate_version, App, Arg};
 use std::str::FromStr;
 use strum;
 use strum_macros::{Display, EnumString};
@@ -6,20 +6,16 @@ use strum_macros::{Display, EnumString};
 fn main() {
     let matches = App::new("sxhkd-mode")
         .version(crate_version!())
-        .arg("<MODE> 'The new sxhkd mode")
+        .arg(
+            Arg::with_name("MODE")
+                .required(true)
+                .possible_values(&["Emacs", "Fundamental", "APL"])
+                .help("The sxhkd mode to set"),
+        )
         .get_matches();
 
-    #[derive(EnumString, Display)]
-    enum Modes {
-        Emacs,
-        Fundemental,
-        APL,
-    };
-
-    let mode = Modes::from_str(matches.value_of("MODE").expect("required")).unwrap_or_else(|_| {
-        eprintln!("Unrecognized mode!");
-        std::process::exit(1);
-    });
-
-    println!("Hello, world? mode: {}", mode);
+    println!(
+        "Hello, world? mode: {}",
+        matches.value_of("MODE").expect("required")
+    );
 }
